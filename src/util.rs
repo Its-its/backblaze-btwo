5ed69861ab99ef672d9d1ad8f65abb0b55ec3109
Its <2779546+Its-its@users.noreply.github.com>
2023-01-27 04:23:17 -0800

    Update encode_file_name

--- src/util.rs
+++ src/util.rs
@@ -2,18 +2,33 @@ pub const API_URL_V2: &str = "https://api.backblazeb2.com/b2api/v2";
 
 
 // Names can be pretty much any UTF-8 string up to 1024 bytes long. There are a few picky rules:
-// No character codes below 32 are allowed.
-// Backslashes are not allowed.
-// DEL characters (127) are not allowed.
-// File names cannot start with "/", end with "/", or contain "//".
 pub fn encode_file_name(file_name: &str) -> String {
     let mut file_name = file_name
-        .replace('\\', "/")
-        .replace("//", "--")
-        .replace(' ', "%20");
+    // Backslashes are not allowed.
+    .replace('\\', "/")
+    .replace("//", "--")
+    .replace(' ', "%20")
+    // DEL characters (127) are not allowed.
+    .replace('\u{7F}', "-");
+
+    // File names cannot start with "/", end with "/", or contain "//".
+    if file_name.ends_with('/') {
+        while file_name.ends_with('/') {
+            file_name.pop();
+        }
+    }
 
     if file_name.starts_with('/') {
-        file_name.remove(0);
+        while file_name.starts_with('/') {
+            file_name.remove(0);
+        }
+    }
+
+    // No character codes below 32 are allowed.
+    for (index, cha) in file_name.clone().char_indices().rev() {
+        if cha < '\u{20}' {
+            file_name.remove(index);
+        }
     }
 
     file_name
